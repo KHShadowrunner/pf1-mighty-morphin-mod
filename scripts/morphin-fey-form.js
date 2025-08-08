cb960db079de48f082c7deb828b378fcad20af79
KHShadowrunner <khutley@gmail.com>
2025-08-08 19:48:32 -0400

    Create morphin-fey-form.js
    Start of adding Fey Form (1-4) to the list

---
+++ scripts/morphin-fey-form.js
@@ -0,0 +1,141 @@
+import { MorphinChanges } from './morphin-changes.js';
+import { MorphinOptions } from './morphin-options.js';
+import { MorphinPolymorphDialog } from './morphin-polymorph-dialog.js';
+
+/**
+ * Application for selecting a shape from the Fey Form spell to change into and then applying that shape to an actor
+ */
+export class MorphinFeyForm extends MorphinPolymorphDialog {
+    /**
+     * @inheritdoc
+     * @param {number} level The level of elemental body to use 1 - 4
+     * @param {string} actorId The id of the actor that will change shape
+     * @param {string} source The source of the elemental body effect
+     */
+    constructor(level, durationLevel, actorId, source, nameOverride, {planarType = null, energizedTypes = null, mutatedType = null} = {}) {
+        super(level, durationLevel, actorId, source, nameOverride, {planarType: planarType, energizedTypes: energizedTypes, mutatedType: mutatedType});
+        this.spell = 'feyForm';
+
+        // Add all possible sizes for the given spell level
+        switch (level) {
+            case 4:
+                this.sizes.elemental = ['sm', 'med', 'lg', 'huge'];
+                break;
+            case 3:
+                this.sizes.elemental = ['sm', 'med', 'lg'];
+                break;
+            case 2:
+                this.sizes.elemental = ['sm', 'med'];
+                break;
+            case 1:
+                this.sizes.elemental = ['sm'];
+                break;
+        }
+
+        this.shapeOptions = {};
+        // Find options to shapeshift into based on the valid size choices above and sort them alphabetically
+        this.shapeOptions.elemental = MorphinOptions.elemental.filter(o => this.sizes.elemental.includes(o.size));
+        this.shapeOptions.elemental.sort((a, b) => { return a.name > b.name ? 1 : -1; });
+    }
+
+    /** @inheritdoc */
+    static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            classes: ['mightyMorphinDialog'],
+            popOut: true,
+            template: 'modules/pf1-mighty-morphin/templates/feyFormDialog.html',
+            id: 'mighty-morphin-feyForm',
+            title: game.i18n.localize('MMMOD.UI.ElementalDialogName'),
+            resizable: false,
+            width: 550
+        });
+    }
+
+    /** @inheritdoc */
+    async getData() {
+        const data = await super.getData();
+
+        // Set the default size to the largest available animal (default type)
+        let defaultSize = this.sizes.elemental[this.sizes.elemental.length - 1];
+        data.elementalOptions = this.shapeOptions.elemental.filter(o => o.size === defaultSize);
+        // Create radio button data for elemental sizes, set the one for the default size as the default checked button
+        data.elementalSizes = this.sizes.elemental.map(o => { return o === defaultSize ? { label: o, size: CONFIG.PF1.actorSizes[o], default: true } : { label: o, size: CONFIG.PF1.actorSizes[o] }; });
+
+        // Get the elemental that will be the first shown in the form dropdown and build the preview of the changes the form will provide
+        data.defaultChoice = data.elementalOptions[0];
+        data.previewHtml = await this.buildPreviewTemplate(data.defaultChoice.name, 'elemental');
+
+        return data;
+    }
+
+    /**
+     * Processes and applies all changes from the passed form to the actor
+     * 
+     * @param {object} event The clicked button event
+     * @param {string} chosenForm The name of the form chosen
+     */
+    async applyChanges(event, chosenForm) {
+        super.applyChanges(event, chosenForm);
+    }
+
+    /**
+     * Updates the options in the formSelect select input based on the type radio and the size clicked by the user in the event
+     * 
+     * @param {object} event listener event from sizeSelect radio buttons
+     * @param {object} formSelect The select html object
+     */
+    async updateFormChoices(event, formSelect) {
+        let newOptions = this.shapeOptions.elemental.filter(o => o.size === event.target.value);
+
+        let newHtml = newOptions.map(o => `<option value="${ o.name }">${ o.name }</option>`);
+        formSelect.innerHTML = newHtml;
+    }
+
+    /**
+     * Processes the changes from the selected beast form into a readable preview display html. In the process it validates the changes based on the level of the spell
+     * 
+     * @param {string} chosenForm The name of the form chosen in the dropdown
+     * @returns {string} HTML containing preview of all the changes to be made to the actor
+     */
+    async buildPreviewTemplate(chosenForm) {
+        let data = {};
+        this.chosenForm = this.shapeOptions.elemental.find(o => o.name === chosenForm);
+
+        this.formData = foundry.utils.duplicate(MorphinChanges.changes[this.chosenForm.name]);
+
+        // Process stat changes for polymorphing smaller than small or larger than medium
+        data.polymorphBase = this.processPolymorphChanges();
+
+        // Process stat changes from the spell based on spell level
+        let chosenElement = chosenForm.split(' ')[1].toLowerCase();
+        this.changes = foundry.utils.duplicate(MorphinChanges.changes[this.spell][chosenElement][this.level].changes);
+        data.scoreChanges = this.processScoreChanges();
+
+        // Process changes to speed, limited by maximum the spell level allows
+        const speeds = this.formData.speed;
+        data.speedChanges = this.processSpeeds(speeds);
+        
+        // Process the natural attacks
+        data.attacks = this.processAttacks();
+
+        // Process the natural attacks
+        this.formData.specialAttack = this.formData.specialAttack?.filter(o => MorphinChanges.allowedSpecials[this.spell][this.level].some(p => o.name === p || o.special?.includes(p))) || [];
+        data.specialAttacks = this.processSpecialAttacks();
+
+        // Process changes in senses limited by the spell level
+        data.senses = this.processSenses();
+
+        // Process special qualities
+        data.special = this.processSpecials();
+
+        mergeObject(data, this.processAttributes());
+
+        // Build the html preview
+        return this.buildHtml(data);
+    }
+
+    /** @inheritdoc */
+    activateListeners(html) {
+        super.activateListeners(html);
+    }
+}
